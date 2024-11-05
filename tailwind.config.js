a5101cc9c32a53a6a1395cccfeb6e7d58d4a4e20
Saranraj S <developer2.git@gmail.com>
2024-11-05 14:31:09 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        default:"#272829"
+      }
+    },
+  },
+  plugins: [],
+}
+
